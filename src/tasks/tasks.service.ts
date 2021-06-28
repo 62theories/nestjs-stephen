ec48e00e60b9d6ab1f1783f76a40aef4449762ba
ball <nattawatx86@gmail.com>
2021-06-28 14:18:50 +0700

    การ validate req

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { Task, TaskStatus } from './tasks.model';
 import { v4 as uuid } from 'uuid';
 import { CreateTaskDto } from './dto/create-task.dto';
@@ -42,15 +42,20 @@ export class TasksService {
   }
 
   getTaskById(id: string): Task {
-    return this.tasks.find((task) => task.id === id);
+    const foundTask = this.tasks.find((task) => task.id === id);
+    if (!foundTask) {
+      throw new NotFoundException('Task not found');
+    }
+    return foundTask;
   }
 
   deleteTaskById(id: string): void {
-    this.tasks = this.tasks.filter((task) => task.id !== id);
+    const foundTask = this.getTaskById(id);
+    this.tasks = this.tasks.filter((task) => task.id !== foundTask.id);
   }
 
   updateTaskStatusById(id: string, status: TaskStatus): Task {
-    const taskFound: Task = this.tasks.find((task) => task.id === id);
+    const taskFound: Task = this.getTaskById(id);
     taskFound.status = status;
     return taskFound;
   }
