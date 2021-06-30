a6040d80857c03d412386c2de8b813aaf8d65845
ball <nattawatx86@gmail.com>
2021-06-30 10:56:08 +0700

    การ config ssl db + port default

--- src/config.schema.ts
+++ src/config.schema.ts
@@ -3,8 +3,9 @@ import * as Joi from '@hapi/joi';
 export const configValidationSchema = Joi.object({
   NODE_ENV: Joi.string().required(),
   DB_HOST: Joi.string().required(),
-  DB_PORT: Joi.number().default(5432).required(),
+  DB_PORT: Joi.number().default(5432),
   DB_USERNAME: Joi.string().required(),
   DB_PASSWORD: Joi.string().required(),
   DB_NAME: Joi.string().required(),
+  PORT: Joi.number().default(3000),
 });
